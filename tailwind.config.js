d7e83532ca856e56ddecf96bfcae5c494555a8e7
Henriquedev0 <henriquerocha.dev@gmail.com>
2024-04-18 19:41:19 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'yellow-custom': 'hsl(47, 88%, 63%)',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
