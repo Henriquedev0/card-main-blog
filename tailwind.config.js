14826afd530055867a4b7d8a6599ea825c6a003e
Henriquedev0 <henriquerocha.dev@gmail.com>
2024-04-18 21:24:35 -0300

    styles and configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ module.exports = {
     extend: {
       colors: {
         'yellow-custom': 'hsl(47, 88%, 63%)',
+      },
+      height: {
+        'custom': '528px', '28': '28px'
+      },
+      width: {
+        '82': '82px',
       }
-
     },
   },
   plugins: [],
